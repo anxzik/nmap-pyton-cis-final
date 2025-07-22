42218739dbefe5d74bc2613611c40f1ae68d3bcb
Aaron Archer <aaron.archer.emt@gmail.com>
2025-07-21 18:51:00 -0600

    Start of Proj d1

---
+++ landing.py
@@ -0,0 +1,14 @@
+#Landing for my Python NMAP project
+import nmap3
+nm = nmap3.NmapScanTechniques()
+results = nmap.nmap_syn_scan()
+
+nmtcp = nmap3.NmapScanTechniques()
+results = nmap.nmap_tcp_scan()
+
+nmfin = nmap3.NmapScanTechniques()
+results = nmap.nmap_fin_scan()
+
+def greeting(menu,nav)
+port_scan =
+    print("Hello, Welcome to my NMAP Scanner,{ ")
